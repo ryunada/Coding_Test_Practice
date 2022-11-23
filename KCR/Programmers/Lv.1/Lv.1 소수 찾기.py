65010eafa4e80b3b3b6c4817bfc17d9ee10cc01b
Kim-Chae-Rin <rin1222k@naver.com>
2022-11-24 00:17:14 +0900

    코딩테스트 연습 > 연습문제 > 소수 찾기

--- KCR/Programmers/Lv.1/Lv.1 소수 찾기.py
+++ KCR/Programmers/Lv.1/Lv.1 소수 찾기.py
@@ -1,10 +1,11 @@
 # '에라토스테네스의 체' 활용
+import math
 def solution(n):
     answer = 0
-    end = int(n**0.5)
-    all = [True] * n # 2~n
+    end = math.ceil(n**0.5)
+    all = [True] * n # 1~n
     for i in range(2, end+1) :
-        if all[i] == True :
-            for j in range(i+i, n, i) :
-                all[j] = False
-    return [i for i in range(2, n) if all[i] == True]
+        if all[i-1] == True : # 리스트는 0부터 시작이므로
+            for j in range(i+i, n+1, i) :
+                all[j-1] = False
+    return len([i for i in range(1, n) if all[i] == True])
