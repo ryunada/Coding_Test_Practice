58a94fb726182cc1310e8fb41ba15ed4a52c1065
ryunada <rst3047@gmail.com>
2022-11-26 23:55:49 +0900

    23..

--- Ryu/Programmers/Lv0/k의 개수.py
+++ Ryu/Programmers/Lv0/k의 개수.py
@@ -1,12 +1,21 @@
-def solution(i, j, k):
-    answer = 0
-    return answer
+# def solution(i, j, k):
+#     answer = 0
+#     for a in range(i, j + 1):
+#         a = str(a)
+#         a = list(a)
+#         b = a.count(str(k))
+#         answer += b
+#     return answer
 
+def solution(i,j,k):
+    return sum([ str(i).count(str(k)) for i in range(i,j+1)])
 
 i = 1
 j = 13
 k = 1
-
+result = 0
 for a in range(i,j+1):
+    a = str(a)
     a = list(a)
-    print(a)
+    b = a.count(k)
+    result += b
