72331027a44ce663c700c11f5896d58c4968acdf
ryunada <rst3047@gmail.com>
2023-03-13 14:35:28 +0900

    sorted

--- Ryu/Programmers/python/Lv0/A로 B 만들기.py
+++ Ryu/Programmers/python/Lv0/A로 B 만들기.py
@@ -1,32 +1,7 @@
 before = "olleh"
 after = "hello"
 
-a = list(before)
-b = list(after)
-# for i in range(len(a)):
-#     for j in range(len(a)):
-#         if a[i] == b[j]:
-#             print(a[i])
-#             a.remove(a[i])
-#             break
-#     print(f"{i}번째 {a}")
-print(a)
-del a[3]
-print(a)
-
-k = 0
-for i in range(len(a)):
-    # print(i)
-    for j in range(len(b)):
-        print(f"i: {i} |j : {j}")
-        if a[i] == b[j]:
-            print(a)
-            del a[i]
-            k += 1
-            break
-
-# if k == len(a):
-#     return 1
-# else:
-#     return 0
-
+if sorted(before) == sorted(after):
+    return 1
+else:
+    return 0
