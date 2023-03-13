dfba06dd12be8e743528393c55688f7d5b137fb5
ryunada <rst3047@gmail.com>
2023-03-13 14:27:30 +0900

    SQL

--- Ryu/Programmers/python/Lv0/A로 B 만들기.py
+++ Ryu/Programmers/python/Lv0/A로 B 만들기.py
@@ -1,5 +1,32 @@
 before = "olleh"
 after = "hello"
 
-for i in before:
-    print(i)
+a = list(before)
+b = list(after)
+# for i in range(len(a)):
+#     for j in range(len(a)):
+#         if a[i] == b[j]:
+#             print(a[i])
+#             a.remove(a[i])
+#             break
+#     print(f"{i}번째 {a}")
+print(a)
+del a[3]
+print(a)
+
+k = 0
+for i in range(len(a)):
+    # print(i)
+    for j in range(len(b)):
+        print(f"i: {i} |j : {j}")
+        if a[i] == b[j]:
+            print(a)
+            del a[i]
+            k += 1
+            break
+
+# if k == len(a):
+#     return 1
+# else:
+#     return 0
+
