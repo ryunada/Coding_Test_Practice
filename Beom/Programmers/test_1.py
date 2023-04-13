d2a816644d12c1461c097ee5d69d2036650985f8
beom <data.beom@gmail.com>
2023-04-13 12:20:40 +0900

    LV0. 가위 바위 보

--- Beom/Programmers/test_1.py
+++ Beom/Programmers/test_1.py
@@ -1,2 +1,10 @@
-print('rear')
-print('beom')
+def solution(rsp):
+    answer = ''
+    for i in range(len(rsp)):
+        if rsp[i]=="0":
+            answer += "5"
+        elif rsp[i]=="2":
+            answer += "0"
+        elif rsp[i]=="5":
+            answer += "2"
+    return answer
