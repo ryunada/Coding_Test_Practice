d0af1229999c61daf102ecacb2cd90d2cac3216a
ryu <rst3047@gmail.com>
2023-04-13 12:57:27 +0900

    commit

--- Ryu/Programmers/python/미해결/숨어있는 숫자의 덧셈 (2).py
+++ Ryu/Programmers/python/미해결/숨어있는 숫자의 덧셈 (2).py
@@ -1,18 +1,12 @@
 my_string = "aAb1B2cC34oOp"
 result = 0
-# num = list()
-# for i in my_string:
-#     if i.isdigit() == 0:
-#         print(num)
-#         # result += int(num)
-#         num = list()
-#         continue
-#     num.append(int(i))
+num = list()
+for i in my_string:
+    if i.isdigit() == 0:
+        print(num)
+        # result += int(num)
+        num = list()
+        continue
+    num.append(int(i))
 # print(num)
 
-a = list('34')
-sum = 0
-for i in a:
-    i = int(i) * 10
-    sum += i
-    print(sum)
