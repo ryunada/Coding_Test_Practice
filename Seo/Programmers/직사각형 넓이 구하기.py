ffec38777d3969472290b0637e5e498543a5c05b
SeoSinyoung <sjh33994@gmail.com>
2023-04-28 14:46:07 +0900

    직사각형 넓이 구하기 + 다른 풀이 추가

--- Seo/Programmers/직사각형 넓이 구하기.py
+++ Seo/Programmers/직사각형 넓이 구하기.py
@@ -9,5 +9,12 @@ def solution(dots):
     return (max[0]-min[0])*(max[1]-min[1])
 
 # print(solution([[1,1],[2,1],[2,2],[1,2]]))
-# dots = [[1,1],[2,1],[2,2],[1,2]]
 
+# 다른 풀이
+def solution(dots):
+    sorted_dots = sorted(dots)
+    width = sorted_dots[3][0] - sorted_dots[0][0]
+    length = sorted_dots[3][1] - sorted_dots[0][1]
+    return width * length
+
+# print(solution([[1,1],[2,1],[2,2],[1,2]]))
