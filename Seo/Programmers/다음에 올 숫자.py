a9cb3059a22ede1a2d162a0b63e30919dfa55da7
SeoSinyoung <sjh33994@gmail.com>
2023-05-09 12:33:48 +0900

    종이 자르기

---
+++ Seo/Programmers/다음에 올 숫자.py
@@ -0,0 +1,16 @@
+def solution(common):
+    d = None
+    r = None
+    if common[1] - common[0] == common[2] - common[1]:
+        d = common[1] - common[0]
+    elif common[1] // common[0] == common[2] // common[1]:
+        r = common[1] // common[0]
+
+    if d is None:
+        answer = common[len(common) - 1] * r
+    else:
+        answer = common[len(common) - 1] + d
+
+    return answer
+
+print(solution([2,4,8]))
