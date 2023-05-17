1114dcf705e7e2c56ac4947aa7b1c5cea85afe6d
SeoSinyoung <sjh33994@gmail.com>
2023-05-17 23:34:11 +0900

    무작위로 k개의 수 뽑기

--- Seo/Programmers/무작위로 k개의 수 뽑기.py
+++ Seo/Programmers/무작위로 k개의 수 뽑기.py
@@ -1,12 +1,12 @@
 def solution(arr, k):
     answer = [arr[0]]
     for i in arr:
-        if i in answer:
+        if i in answer:            # 나온 적이 있는 수: pass
             continue
-        else:
+        else:                      # 나온 적이 없는 수: 길이 k될 때 까지 배열 뒤에 추가
             if len(answer) < k:
                 answer.append(i)
-    while len(answer) < k:
+    while len(answer) < k:         # 위에서 만든 배열의 길이 < k 이면 나머지 -1로 채워
         answer.append(-1)
     return answer
 
