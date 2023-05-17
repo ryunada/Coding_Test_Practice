2c278fe904ced2b7780a302df33ad20b45a03bf2
SeoSinyoung <sjh33994@gmail.com>
2023-05-17 23:47:53 +0900

    옹알이(1)

--- Seo/Programmers/옹알이(1).py
+++ Seo/Programmers/옹알이(1).py
@@ -4,13 +4,13 @@ def solution(babbling):
         #print("babbling=",i)
         for j in ["aya", "ye", "woo", "ma"]:
             #print("words=",j)
-            while (j in i):
-                i = i.replace(j," ")
+            while (j in i):                         # 옹알이 안에 네 가지 발음이 중 하나가 있다면 공백으로 대체
+                i = i.replace(j," ")                # "wyeoo" -> "w oo" , 발음만으로 구성됐다면 모두 공백
                 #print("babbling=",i)
-                if i.replace(" ","") == "":
+                if i.replace(" ","") == "":         # 공백 여러 개를 하나로 다루기 위해
                     answer += 1
                     #print(answer)
                     break
     return answer
 
-print(solution(["aya", "yee", "u", "maa", "wyeoo"]))
+print(solution(["ayaye", "uuuma", "ye", "yemawoo", "ayaa"]))
