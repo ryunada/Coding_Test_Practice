5aa3405ae8fdabaa64a0d2f3412334ee93436200
ryunada <rst3047@gmail.com>
2023-06-01 10:39:22 +0900

    테스트 9번

--- Ryu/Programmers/python/미해결/안전지대.py
+++ Ryu/Programmers/python/미해결/안전지대.py
@@ -77,94 +77,94 @@
 
 # board = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 1, 0, 0], [0, 0, 0, 0, 0]]
 # board = [[0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 0], [0, 0, 1, 1, 0], [0, 0, 0, 0, 0]]
-board = [[1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]]
+# board = [[1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1], [1, 1, 1, 1, 1, 1]]
+board = [[0,0,0],[0,0,0],[0,0,1]]
 def solution(board):
     test = [[0] * len(board) for i in range(len(board))]
     answer = 0
-    if len(board) == 1:
-        return board[0]
+    if len(board) == 1:   # 배열이 1*1인 경우
+        if board[0] == 1: # 1배열에 폭탄이 있으면 안전 지대는 0
+            return 0
+        else:             # 배열에 폭탄이 없으면 안전지대는 1
+            return 1
+    elif len(board) == 2: # 배열이 2*2인 경우
+        for i in range(len(board)):
+            for j in range(len(board)):
+                if board[i][j] == 1:
+                    return 0
+        return 4
+    else:
+        for i in range(len(board)):
+            for j in range(len(board)):
+                if board[i][j] == 1:
+                    if (i == 0) & (j == 0): # 왼쪽 위 모서리
+                        print(f"i : {i} j: {j}")
+                        test[i][j] += 1
+                        test[i][j+1] += 1
+                        test[i+1][j+1] += 1
+                        test[i+1][j] += 1
+                    if (i == 0) & (0 < j < len(board)-1): # 모퉁이 제외 위쪽 벽면
+                        print(f"i : {i} j: {j}")
+                        test[i][j] += 1
+                        test[i][j-1] += 1
+                        test[i+1][j-1] += 1
+                        test[i+1][j] += 1
+                        test[i+1][j+1] += 1
+                        test[i][j+1] += 1
+                    if (i == 0) & (j == len(board)-1): # 오른쪽 위 모퉁이
+                        print(f"i : {i} j: {j}")
+                        test[i][j] += 1
+                        test[i][j-1] += 1
+                        test[i+1][j-1] += 1
+                        test[i+1][j] += 1
+                    if (0 < i < len(board)-1) & (j == 0): # 모퉁이 제외 왼쪽 벽면
+                        print(f"i : {i} j: {j}")
+                        test[i][j] += 1
+                        test[i-1][j] += 1
+                        test[i-1][j+1] += 1
+                        test[i][j+1] += 1
+                        test[i+1][j] += 1
+                        test[i+1][j+1] += 1
+                    if (0 < i < len(board)-1) & (j == len(board)-1): # 모퉁이 제외 오른쪽 벽면
+                        print(f"i : {i} j: {j}")
+                        test[i][j] += 1
+                        test[i-1][j-1] += 1
+                        test[i-1][j] += 1
+                        test[i][j-1] += 1
+                        test[i+1][j-1] += 1
+                        test[i+1][j] += 1
+                    if (i == len(board)-1) & (j == 0): # 왼쪽 아래 모퉁이
+                        print(f"i : {i} j: {j}")
+                        test[i][j] += 1
+                        test[i-1][j] += 1
+                        test[i-1][j+1] += 1
+                        test[i][j+1] += 1
+                    if (i == len(board)-1) & (0 < j < len(board)-1): # 모퉁이 제외 아래 벽면
+                        print(f"i : {i} j: {j}")
+                        test[i][j] += 1
+                        test[i-1][j-1] += 1
+                        test[i-1][j] +=1
+                        test[i-1][j+1] += 1
+                        test[i][j-1] += 1
+                        test[i][j+1] += 1
+                    if (i == len(board)-1) & (j == len(board)-1): # 오른쪽 아래 모퉁이
+                        print(f"i : {i} j: {j}")
+                        test[i][j] += 1
+                        test[i-1][j-1] += 1
+                        test[i-1][j] += 1
+                        test[i][j-1] += 1
+                    if (0 < i < len(board)-1) & (0 < j < len(board)-1): # 제외 없음
+                        print(f"i : {i} j: {j}")
+                        test[i][j] += 1
+                        test[i-1][j-1] += 1
+                        test[i-1][j] += 1
+                        test[i-1][j+1] += 1
+                        test[i][j-1] += 1
+                        test[i][j+1] += 1
+                        test[i+1][j-1] += 1
+                        test[i+1][j] += 1
+                        test[i+1][j+1] += 1
 
-    for i in range(len(board)):
-        for j in range(len(board)):
-            # print(f"{i} | {j} | {board[i][j]}")
-            if board[i][j] == 1:
-                if (i == 0) & (j == 0): # 왼쪽 위 모서리
-                    print(f"i : {i} j: {j}")
-                    test[i][j] += 1
-                    test[i][j+1] += 1
-                    test[i+1][j+1] += 1
-                    test[i+1][j] += 1
-                    # break
-                if (i == 0) & (0 < j < len(board)-1): # 모퉁이 제외 위쪽 벽면
-                    print(f"i : {i} j: {j}")
-                    test[i][j] += 1
-                    test[i][j-1] += 1
-                    test[i+1][j-1] += 1
-                    test[i+1][j] += 1
-                    test[i+1][j+1] += 1
-                    test[i][j+1] += 1
-                    # break
-                if (i == 0) & (j == len(board)): # 오른쪽 위 모퉁이
-                    print(f"i : {i} j: {j}")
-                    test[i][j] += 1
-                    test[i][j-1] += 1
-                    test[i+1][j-1] += 1
-                    test[i+1][j] += 1
-                    # break
-                if (0 < i < len(board)-1) & (j == 0): # 모퉁이 제외 왼쪽 벽면
-                    print(f"i : {i} j: {j}")
-                    test[i][j] += 1
-                    test[i-1][j] += 1
-                    test[i-1][j+1] += 1
-                    test[i][j+1] += 1
-                    test[i+1][j] += 1
-                    test[i+1][j+1] += 1
-                    # break/ /
-
-                if (0 < i < len(board)-1) & (j == len(board)): # 모퉁이 제외 오른쪽 벽면
-                    print(f"i : {i} j: {j}")
-                    test[i][j] += 1
-                    test[i-1][j-1] += 1
-                    test[i-1][j] += 1
-                    test[i][j-1] += 1
-                    test[i+1][j-1] += 1
-                    test[i+1][j] += 1
-                    # break
-                if (i == len(board)) & (j == 0): # 왼쪽 아래 모퉁이
-                    print(f"i : {i} j: {j}")
-                    test[i][j] += 1
-                    test[i-1][j] += 1
-                    test[i-1][j+1] += 1
-                    test[i][j+1] += 1
-                    # break
-                if (i == len(board)) & (0 < j < len(board)-1): # 모퉁이 제외 아래 벽면
-                    print(f"i : {i} j: {j}")
-                    test[i][j] += 1
-                    test[i-1][j-1] += 1
-                    test[i-1][j] +=1
-                    test[i-1][j+1] += 1
-                    test[i][j-1] += 1
-                    test[i][j+1] += 1
-                    # break
-                if (i == len(board)) & (j == len(board)): # 오른쪽 아래 모퉁이
-                    print(f"i : {i} j: {j}")
-                    test[i][j] += 1
-                    test[i-1][j-1] += 1
-                    test[i-1][j] += 1
-                    test[i][j-1] += 1
-                    # break
-                if (0 < i < len(board)-1) & (0 < j < len(board)-1) & (len(board) > 2): # 제외 없음
-                    print(f"i : {i} j: {j}")
-                    test[i][j] += 1
-                    test[i-1][j-1] += 1
-                    test[i-1][j] += 1
-                    test[i-1][j+1] += 1
-                    test[i][j-1] += 1
-                    test[i][j+1] += 1
-                    test[i+1][j-1] += 1
-                    test[i+1][j] += 1
-                    test[i+1][j+1] += 1
-                    # break
 
     for i in range(0,len(board)):
         for j in range(0, len(board)):
