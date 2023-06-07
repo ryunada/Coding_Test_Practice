1b4684f6fc4d54777494b171be0498a83895456b
ryunada <rst3047@gmail.com>
2023-06-08 00:37:50 +0900

    특이한 정렬

--- Ryu/Programmers/python/Lv0/특이한 정렬.py
+++ Ryu/Programmers/python/Lv0/특이한 정렬.py
@@ -1,20 +1,34 @@
-# numlist = [1,2,3,4,5,6]
-# n = 4
-numlist = [10000,20,36,47,40,6,10,7000]
-n = 30
+numlist = [1,2,3,4,5,6]
+n = 4
+# numlist = [10000,20,36,47,40,6,10,7000]
+# n = 30
 # def solution(numlist, n):
-result = list() # 결과
-numlist_f = list() # n 이전
-numlist_l = list() # n 이후 (n 포함)
-numlist.sort()
 
-for i in numlist:
-    if i < n:
-        numlist_f.append(i) # n 이전
-    else:
-        numlist_l.append(i) # n 이후(n 포함)
+def solution(numlist, n):
+    numlist_f = list()  # n 이전
+    numlist_l = list()  # n 이후 (n 포함)
+    numlist.sort()
 
-print(f"앞 : {numlist_f}")
-print(f"뒤 : {numlist_l}")
+    for i in numlist:
+        if i >= n:
+            numlist_l.append(i)  # n 이후(n 포함)
+        else:
+            numlist_f.append(i)  # n 이전
 
-# solution(numlist, n)
+    for i in range(len(numlist_f) - 1, -1, -1):
+        for j in range(len(numlist_l)):
+            if abs(numlist_l[j] - n) > abs(numlist_f[i] - n):
+                numlist_l.insert(j, numlist_f[i])
+                numlist_f.remove(numlist_f[i])
+                break
+    for i in range(len(numlist_f) - 1, -1, -1):
+        numlist_l.append(numlist_f[i])
+    return numlist_l
+
+
+
+
+
+
+
+
