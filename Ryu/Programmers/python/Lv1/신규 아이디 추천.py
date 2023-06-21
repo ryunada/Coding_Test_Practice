fa83b2feb9a8f65ee868cbfb69401d55b64e5a7c
ryunada <rst3047@gmail.com>
2023-06-22 00:50:05 +0900

    신규 아이디 추천

--- Ryu/Programmers/python/Lv1/신규 아이디 추천.py
+++ Ryu/Programmers/python/Lv1/신규 아이디 추천.py
@@ -4,6 +4,8 @@ new_id = ''
 # new_id = 'z-+.^.'
 
 print(f"입력한 ID : {new_id}")
+
+
 def solution(new_id):
     # 조건 1. 소문자로 치환
     one_id = new_id.lower()
@@ -18,15 +20,17 @@ def solution(new_id):
     three_id = ''
     two_id_l = list(two_id)
 
-    for i in range(1, len(two_id_l)):
-        if (two_id_l[i - 1] == '.') & (two_id_l[i] == '.'):
-            continue
-        three_id += two_id_l[i - 1]
+    if len(two_id_l) >= 1:
+        for i in range(1, len(two_id_l)):
+            if (two_id_l[i - 1] == '.') & (two_id_l[i] == '.'):
+                continue
+            three_id += two_id_l[i - 1]
 
     # 조건 4. 앞에 뒤에 마침표(.)이면 제거 | 조건 3에서 뒤에 마침표가 있으면 없앰
     four_id = ''
-    if three_id[0] == '.':
-        four_id = three_id[1:]
+    if len(three_id) >= 1:
+        if three_id[0] == '.':
+            four_id = three_id[1:]
 
     # 조건 5. new_id가 빈 문자열이라면, new_id에 'a'를 대입
     if len(four_id) == 0:
@@ -37,8 +41,9 @@ def solution(new_id):
         five_id = four_id[:15]
         if five_id[-1] == '.':
             five_id = five_id[:-1]
-
-    # 조건 7.
+    else:
+        five_id = four_id
+        # 조건 7.
     result_id = five_id
     if len(five_id) <= 2:
         while (len(result_id) >= 3):
@@ -46,4 +51,7 @@ def solution(new_id):
 
     return result_id
 
+
 print(solution(new_id))
+
+
