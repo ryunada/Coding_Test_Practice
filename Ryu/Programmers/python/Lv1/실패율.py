c0c279012467627f22d528a8e59759bb08da8da5
ryunada <rst3047@gmail.com>
2023-08-11 00:35:01 +0900

    실패율

--- Ryu/Programmers/python/Lv1/실패율.py
+++ Ryu/Programmers/python/Lv1/실패율.py
@@ -1,18 +1,20 @@
 # 실패율
 # 스테이지에 도달했으나 아직 클리어하지 못한 플레이어의 수 / 스테이지에 도달한 플레이어 수
 
-N = 7
+N = 8
 stages = [2, 1, 2, 6, 2, 4, 3, 3]
 # 런타임 에러
+
 def solution(N, stages):
     Player_N = len(stages) # 참여자?
     dic = {}
     for i in range(1, N+1):
-        FR = stages.count(i) / Player_N  # 실패율
-        print(f"i : {i} | stages_N : {stages.count(i)} |  Player_N : {Player_N} | FR : {FR}")
+        if Player_N != 0:
+            FR = stages.count(i) / Player_N  # 실패율
+        else:
+            FR = 0
         Player_N -= stages.count(i)  # 플에이어 수
         dic[i] = FR
-        print(dic)
     return sorted(dic, key = lambda  x:dic[x], reverse= True)
 
 # def solution(N, stages):
