0e2a379f3b75b7ee38b3bf2383a46c676a24c832
ryunada <rst3047@gmail.com>
2023-08-25 00:32:09 +0900

    test

---
+++ Ryu/Programmers/python/Lv2/괄호 회전하기.py
@@ -0,0 +1,19 @@
+# ex1)
+s = "[](){}"
+
+# ex2)
+# s = "}]()[{"
+
+# ex3)
+# s = "[)(]"
+
+# ex4)
+# s = "}}}"
+
+# def solution(s):
+#     answer = -1
+#     return answer
+
+
+for i in range(len(s)):
+    print(i)
