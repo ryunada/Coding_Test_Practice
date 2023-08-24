39b70b194456010fd708cd7c9a7ca11153e7303f
ryunada <rst3047@gmail.com>
2023-08-25 01:06:50 +0900

    괄호 회전

--- Ryu/Programmers/python/Lv2/괄호 회전하기.py
+++ Ryu/Programmers/python/Lv2/괄호 회전하기.py
@@ -1,8 +1,8 @@
 # ex1)
-s = "[](){}"
+# s = "[](){}"
 
 # ex2)
-# s = "}]()[{"
+s = "}]()[{"
 
 # ex3)
 # s = "[)(]"
@@ -15,5 +15,24 @@ s = "[](){}"
 #     return answer
 
 
+result = 0
 for i in range(len(s)):
-    print(i)
+    print(f"변환 전 : {s}")
+    s_c = s[:]
+    for j in range(len(s)//2):
+        print(f"변환 전 s_c : {s_c}")
+        # 쓸데 없는 메모리 낭비를 줄이기 위해 if 사용
+        if s_c.find('{}') != -1:
+            s_c = s_c.replace('{}', '')
+        elif s_c.find('[]') != -1:
+            s_c = s_c.replace('[]', '')
+        elif s_c.find('()') != -1:
+            s_c = s_c.replace('()', '')
+    print(f"변환 후 s_c : {s_c}")
+    if len(s_c) == 0:
+        result += 1
+    print(s)
+    s += s[0]
+    s = s[1:]
+
+print(result)
