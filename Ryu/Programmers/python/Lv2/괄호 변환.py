c8a2397f5629db5938c3c396f45fcb98dd8d8e40
ryunada <rst3047@gmail.com>
2023-08-28 18:09:48 +0900

    망망망

--- Ryu/Programmers/python/Lv2/괄호 변환.py
+++ Ryu/Programmers/python/Lv2/괄호 변환.py
@@ -14,17 +14,18 @@ for i in range(4):
     print(f"{i+1}번째")
     o_n = 0  # 올바른것
     for i in range(len(p_p)//2):
-        if p_p[0] == '(':     # u : 올바른 괄호 문자열
+        print(p_p[0])
+        if p_p[0] == ')':                    # p[0] == ')'인 경우
+            if p_p.find('()') != -1:         # 올바른 괄호 문자열이 있다면 그 전까지
+                a = 1
+                u = p_p[:p_p.find('()')]
+                v = p_p[p_p.find('()'):]
+            break
+        else:                                # u : 올바른 괄호 문자열
             if p_p.find('()') == 0:
                 o_n += 1
                 p_p = p_p.replace('()','',1)
                 a = 0
-            break
-        else: # p[0] == ')'인 경우 # v : 나머지
-            if p_p.find('()') != -1: # 올바른 괄호 문자열이 있다면 그 전까지
-                a = 1
-                u = p_p[:p_p.find('()')]
-                v = p_p[p_p.find('()'):]
             break
 
     if a == 0:
