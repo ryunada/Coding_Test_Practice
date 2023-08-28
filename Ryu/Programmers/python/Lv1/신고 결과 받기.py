3ffee4a492732e21cf9210dc9d3e72db6029e052
ryunada <rst3047@gmail.com>
2023-08-28 17:29:54 +0900

    조금만 수정을 하면 될거같은데

--- Ryu/Programmers/python/Lv1/신고 결과 받기.py
+++ Ryu/Programmers/python/Lv1/신고 결과 받기.py
@@ -1,43 +1,21 @@
 # ex 1)
-id_list = ["muzi", "frodo", "apeach", "neo"]
-report = ["muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"]
-k = 2
-
+# id_list = ["muzi", "frodo", "apeach", "neo"]
+# report = ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
+# k = 2
 
 # ex 2)
-# id_list = ["con", "ryan"]
-# report = ["ryan con", "ryan con", "ryan con", "ryan con"]
-# k = 3
+id_list = ["con", "ryan"]
+report = ["ryan con", "ryan con", "ryan con", "ryan con"]
+k = 3
+
 def solution(id_list, report, k):
     report = set(report)
     report = list(report)
 
-    # 딕셔너리 초기화
-    Name_dict = {} # 유저 ID당 신고를 한 사람 딕셔너리
-    N_dict = {}    # 신고를 받은 수를 담는 딕셔너리
+    id_list_dict = {}
     for i in id_list:
-        Name_dict[i] = 0
-        N_dict[i] = 0
-    print(f"유저 -> 신고한 사람 : {Name_dict}")
-    print(f"유저 -> 신고 당한 수 : {N_dict}")
-    print("---------------------------------------------------------------------------------------")
-
+        id_list_dict[i] = 0
+    print(id_list_dict)
     for i in range(len(report)):
-        N_dict[report[i].split()[1]] += 1                            # 유저ID -> 신고를 당한 수
-    print(f"유저 -> 신고 당한 수 : {N_dict}")
-
-    # k번 이상이면 메일 발송
-    for i in N_dict:
-        if N_dict[i] >= k:
-            N_dict[i] = 1
-        else:
-            N_dict[i] = 0
-    print(f"유저 -> 신고 당한 수 : {N_dict}")
-
-    print(f"유저 -> 신고한 사람 : {Name_dict}")
-    for i in range(len(report)):
-        if N_dict[report[i].split()[1]] == 1:
-            Name_dict[report[i].split()[0]] += 1 # 유저ID -> 신고를 당한 사람
-    return list(Name_dict.values())
-
-print( solution(id_list, report, k))
+        id_list_dict[report[i].split()[1]] += 1
+    return id_list_dict
--- Ryu/Programmers/python/Lv2/괄호 변환.py
+++ Ryu/Programmers/python/Lv2/괄호 변환.py
@@ -1,15 +1,33 @@
 # ex1)
-p = "(()())()"
+# p = "(()())()"
 
 # ex2)
 # p = ")("
 
 # ex3)
-# p = "()))((()"
+p = "()()))((()"
 
+result = ''
+p_p = p[:]
 
-# 1. 입력이 빈
-result = []
+for i in range(4):
+    print(f"{i+1}번째")
+    o_n = 0  # 올바른것
+    for i in range(len(p_p)//2):
+        if p_p[0] == '(':     # u : 올바른 괄호 문자열
+            if p_p.find('()') == 0:
+                o_n += 1
+                p_p = p_p.replace('()','',1)
+        else: # p[0] == ')'인 경우 # v : 나머지
+            print(p_p)
+            break
 
+    result += p[:o_n * 2]
+    u = p[:o_n * 2]
+    v = p[o_n*2 :]
+    p_p = v[:]
+    print(f"result : {result}")
+    print(f"u : {u}")
+    print(f"v : {v}")
 
-
+# print(solution(p))
